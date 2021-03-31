190bc97b68aefe33020568cb782220e6cfb32ed4
Martin Thuranira <38188355+MartinThuranira@users.noreply.github.com>
2021-03-31 15:14:13 +0300

    Update main.rs

--- main.rs
+++ main.rs
@@ -7,7 +7,6 @@ use stm32f1xx_hal::flash::FlashExt;
 use panic_halt as _;
 use cortex_m_semihosting::{debug,hprintln};
 use rtic::app;
-//use stm32f1::stm32f103;
 use hd44780_driver::{Cursor, CursorBlink, Display, DisplayMode, HD44780};
 use stm32f1xx_hal::{gpio::{gpioa::PA15,Output,PushPull},
                     stm32::*,
@@ -17,20 +16,14 @@ use stm32f1xx_hal::{gpio::{gpioa::PA15,Output,PushPull},
 
 
 #[app(device = stm32f1xx_hal::stm32, peripherals = true)]
-const APP: () = {
-/*
-    struct Resources{
-        delay:Delay,
-    }*/
+const APP: () 
     #[init]
-    fn init(cx:init::Context) //-> init::LateResources 
+    fn init(cx:init::Context)
     {
     
         let cp = cx.core;
-        //let dp = cx.device;
         let dp = stm32f1xx_hal::pac::Peripherals::take().unwrap();
-        //let dp = stm32f1xx_hal::stm32::Peripherals::take().unwrap();
-        //hprintln!("Hello, world!").unwrap();
+       
 
         let mut flash = dp.FLASH.constrain();
         let mut rcc = dp.RCC.constrain();
@@ -52,35 +45,11 @@ const APP: () = {
 
     let mut lcd = HD44780::new_4bit(rs, en, b4, b5, b6, b7, &mut delay);
 
-// Unshift display and set cursor to 0
-//lcd.unwrap().reset(&mut delay); 
 
-// Clear existing characters
-//lcd.expect("invalid conection").clear(&mut delay); 
 
 // Display the following string
 lcd.expect("invalid conection").write_str("Hello, world!", &mut delay);
 
-// Move the cursor to the second line
-//lcd.expect("invalid conection").set_cursor_pos(40, &mut delay);
-
-// Display the following string on the second line
-//lcd.expect("invalid conection").write_str("I'm on line 2!", &mut delay);
-
-
-    // lcd.reset();
-    // lcd.clear();
-    // lcd.set_display_mode(
-    //     DisplayMode {
-    //         display: Display::On,
-    //         cursor_visibility: Cursor::Visible,
-    //         cursor_blink: CursorBlink::On,
-    //     });
-/*
-    init::LateResources{
-        delay,
-    }
-    */
 }
 
     #[idle]
